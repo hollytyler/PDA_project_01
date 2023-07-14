c6fbe477f9bb1840106a6742b564ccee186172e1
Holly Tyler <hollystertyler@gmail.com>
2023-07-14 17:50:07 +0100

    created a list which was passed into a function and then added notes about it all.

---
+++ list.py
@@ -0,0 +1,15 @@
+list_01 = [4, 28, 11, 13, 76, 38, 8]
+
+def sum_of_numbers(list):
+    return sum(list)
+
+print(sum_of_numbers(list_01))
+
+
+# A list is a storage container for objects in a program
+# which are stored as a part of a collection.
+
+# In this function I have created a list and assigned it to a variable
+# I then wanted to find the sum of every item in the list
+# so the defined function "sum_of_numbers" will take in the list 
+# and then retrun the sum. 
